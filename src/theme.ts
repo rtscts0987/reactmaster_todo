616acfc86284604b2f8b0012a451cdafa46ec039
Hyunkuk Kim <60034879+rtscts0987@users.noreply.github.com>
2023-03-02 01:41:50 +0900

    init

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme :DefaultTheme = {
+    bgColor: "#303952",
+    textColor: "#f5cd79",
+    accentColor: "#3dc1d3",
+    // cardBgColor: "transparent",
+}
+
