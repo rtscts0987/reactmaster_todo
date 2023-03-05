989c4d7703e5a696ba98552a4e9706853df5398d
Hyunkuk Kim <60034879+rtscts0987@users.noreply.github.com>
2023-03-05 23:50:07 +0900

    0.7.0

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,32 +1,14 @@
 import { atom, selector } from "recoil";
 
-export enum Categories {
-    "TO_DO" = "TO_DO",
-    "DOING" = "DOING",
-    "DONE" = "DONE",
-  }
-
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-export const categoryState = atom<Categories>({
-    key: "category",
-    default: Categories.TO_DO,
-  });
-
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoSelector = selector({
-    key: "toDoSelector",
-    get: ({ get }) => {
-      const toDos = get(toDoState);
-      const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
-    },
-  });
+export const hourSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+});
